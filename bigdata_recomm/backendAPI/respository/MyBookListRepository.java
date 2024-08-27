23c594899361514871250db1883b89c1e67a1d95
agks <140994215+agks12@users.noreply.github.com>
2024-08-27 17:07:43 +0900

    Create MyBookListRepository.java

---
+++ bigdata_recomm/backendAPI/respository/MyBookListRepository.java
@@ -0,0 +1,13 @@
+package com.example.pppp.Bookfolder.respository;
+
+import com.example.pppp.Bookfolder.entity.MyBookList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface MyBookListRepository extends JpaRepository<MyBookList, Integer> {
+    MyBookList findByBookId(int bookId); // 기본적인 책 정보 가져오기
+    List<MyBookList> findTop100ByOrderByBookIdAsc();
+
+}
